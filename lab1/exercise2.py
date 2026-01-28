67f04424ddb485c2d7f096f1fcf24267f40e63c0
Xiaoan Li <andyli201501@gmail.com>
2026-01-28 11:34:48 +0800

    Complete Lab1 Python exercises

---
+++ lab1/exercise2.py
@@ -0,0 +1,8 @@
+# Write a function that prints all factors of the given parameter x
+def print_factor(x):
+    # your code here
+	for i in range (2, x):
+		if x % i == 0:
+			print (i)
+#test
+print_factor(52633)
